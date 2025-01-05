4ed81577d4f948de084b2616ec66625369ae1b6f
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-05 23:16:46 +0530

    Updates

--- src/Pages/RegisterPage/ClientRegister/MultiStepForm.js
+++ src/Pages/RegisterPage/ClientRegister/MultiStepForm.js
@@ -8,23 +8,20 @@ import CompanyInformation from "./CompanyInformation/CompanyInformation";
 import OnboardingForm from "./OnboardingForm/OnboardingForm";
 
 const MultiStepForm = () => {
-  const [currentStep, setStep] = useState(2);
+  const [currentStep, setStep] = useState(1);
 
   const nextStep = () => {
     if (currentStep < 5) setStep(currentStep + 1);
   };
 
   const prevStep = () => {
-    if (currentStep > 1) setStep(currentStep - 1);
+    if (currentStep === 5) {
+      console.log("Finish clicked");
+      // Add finish logic here
+    } else if (currentStep > 1) {
+      setStep(currentStep - 1);
+    }
   };
-
-  const steps = [
-    "Select your expertise",
-    "Personal Details",
-    "Current address",
-    "Fit to work",
-    "Onboarding Form Complete",
-  ];
 
   return (
     <div>
@@ -34,25 +31,32 @@ const MultiStepForm = () => {
       </div>
 
       <div className="step-selection">
-        {/* Step Content 1 */}
-        {currentStep == 1 && <ExpertiseSelection />}
-
-        {/* Step Content 2 */}
+        {/* Step Content */}
+        {currentStep === 1 && <ExpertiseSelection />}
         {currentStep === 2 && <OrganisationDetails />}
-
-        {/* Step Content 3 */}
         {currentStep === 3 && <CompanyAddress />}
-
-        {/* Step Content 4 */}
         {currentStep === 4 && <CompanyInformation />}
-
-        {/* Step Content 5 */}
         {currentStep === 5 && <OnboardingForm />}
 
         {/* Navigation Buttons */}
-        <div className="form-navigation">
-          {currentStep > 1 && <button onClick={prevStep}>Previous</button>}
-          {currentStep < 5 && <button onClick={nextStep}>Next</button>}
+        <div
+          className={`form-navigation ${
+            currentStep === 1 ? "first-step" : currentStep === 5 ? "last-step" : "default-step"
+          }`}
+        >
+          {currentStep > 1 && currentStep < 5 && (
+            <button onClick={prevStep}>Previous</button>
+          )}
+          {currentStep === 5 && (
+            <button onClick={prevStep} className="finish">
+              Finish
+            </button>
+          )}
+          {currentStep < 5 && (
+            <button onClick={nextStep} className={currentStep === 1 ? "right" : ""}>
+              Next
+            </button>
+          )}
         </div>
       </div>
     </div>
